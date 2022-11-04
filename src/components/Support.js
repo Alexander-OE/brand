b6cf22ca4a970b06b8c73d563d9b91e7d6328590
Egbe Alexander <alexsucceeds@gmail.com>
2022-11-04 20:20:15 +0100

    bbb

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,5 +1,7 @@
 const Support = () => {
-    return ( <div></div> );
+    return ( <div className="text-3xl text-blue-700 ">
+        <h4>Yo whats upjehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h4>
+    </div> );
 }
  
 export default Support;
